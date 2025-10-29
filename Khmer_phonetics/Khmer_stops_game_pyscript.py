3b06603b766d1a5bbc7a3da8aea2aeae637f5355
Oriel <oriel.a.tabilo@gmail.com>
2025-10-29 12:31:50 +1100

    make the buttons work

--- Khmer_phonetics/Khmer_stops_game_pyscript.py
+++ Khmer_phonetics/Khmer_stops_game_pyscript.py
@@ -1,22 +1,54 @@
-from pyscript import document
+import random
+from enum import IntEnum
+
+from pyscript.web import page, when
 
 from alphabet import KhmerAlphabet
 
-def main():
+class Correctness(IntEnum):
     """
-    The main function, that runs when the page is loaded
+    Map correctness values to int
     """
-    # populate the Khmer alphabet once
-    A = KhmerAlphabet()
+    Incorrect = 0
+    Correct = 1
 
-    option1 = document.querySelector("#option1")
-    option2 = document.querySelector("#option2")
+def set_options():
+    """
+    Select two letters from the Khmer alphabet for the option buttons
+    """
+    # get the buttons
+    option1 = page["#option1"]
+    option2 = page["#option2"]
 
     # select random choices for the buttons
     choices = A.choose(param=2, value=1)
-    option1.innerText = chr(choices[0]) # display unicode codepoint
-    option2.innerText = chr(choices[1])
+    flip = [Correctness.Incorrect, Correctness.Correct] # choices[1] is correct, before shuffling
+    random.shuffle(flip)
 
+    # display unicode codepoint
+    option1.innerText = chr(choices[flip[0].value])
+    option2.innerText = chr(choices[flip[1].value])
 
-# when the page is loaded, call the main function
-main()
+    # Add "Correct" or "Incorrect" class names
+    option1.classes.add(flip[0].name)
+    option2.classes.add(flip[1].name)
+
+def submit(event):
+    """
+    When on of the option buttons is clicked
+    """
+    # get button's "Correct" or "Incorrect" class
+    button = event.target
+    print("Button clicked", button.id)
+    correctness = button.classList
+
+    # display mark
+    mark = page["#mark"]
+    mark.classList = "" # remove existing classes
+    mark.classes.add(correctness) # add class for CSS
+    mark.innerHTML = f"{correctness}!"
+    print("Mark:", correctness)
+
+# ON PAGE LOAD
+A = KhmerAlphabet() # load the Khmer alphabet once
+set_options() # set the option buttons
