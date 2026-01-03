e017b5e6f6b34cfb6ec03007723a0ee097030036
Oriel <oriel.a.tabilo@gmail.com>
2026-01-03 21:36:29 +1100

    conjugate verbs

--- Bangla_conjugation/bangla_conjugation.py
+++ Bangla_conjugation/bangla_conjugation.py
@@ -1,35 +1,45 @@
-import csv, dataclass
+import csv, os
 
 from pyscript.web import page, when
 
 ENDINGS = {
     "PRES": {
-        "1": "i",
-        "2_inf": "o",
-        "2_fam": "is",
-        "3": "e",
-        "23_form": "en",
+        "1": "ি",
+        "2_inf": "ো",
+        "2_fam": "িস",
+        "3": "ে",
+        "23_form": "েন",
     },
     "PAST": {
-        "1": "lam",
-        "2_inf": "le",
-        "2_fam": "li",
-        "3": "lo",
-        "23_form": "len",
+        "1": "লাম",
+        "2_inf": "লে",
+        "2_fam": "লি",
+        "3": "লো", # explicit inherent vowel
+        "23_form": "লেন",
     },
     "FUT": {
-        "1": "bo",
-        "2_inf": "be",
-        "2_fam": "bi",
-        "3": "be",
-        "23_form": "ben",
+        "1": "বো", # explicit inherent vowel
+        "2_inf": "বে",
+        "2_fam": "বি",
+        "3": "বে",
+        "23_form": "বেন",
     },
     "HAB": {
-        "1": "i",
-        "2_inf": "o",
-        "2_fam": "is",
-        "3": "e",
-        "23_form": "en"
+        "1": "তাম",
+        "2_inf": "তে",
+        "2_fam": "তি",
+        "3": "তো", # explicit inherent vowel
+        "23_form": "তেন",
+    },
+}
+ASPECTS = {
+    "PRES": {
+        "PRES_CONT": "ছ",
+        "PRES_PERF": "েছ"
+    },
+    "PAST": {
+        "PAST_CONT": "ছি",
+        "PAST_PERF": "েছি"
     }
 }
 
@@ -43,38 +53,44 @@ def make_ready():
     vertical = page["#vertical"]
     vertical.classes.remove("loading")
 
-class Verb():
-    """
-    An object that represents a verb from Bangla with all of its conjugations
-    """
-    def __init__(self, verbal_noun, English):
-        self.verbal_noun = verbal_noun
-        self.root = verbal_noun[:-1]
-        self.english = English
-        self.forms = self._conjugate(self.root)
-
-    def _conjugate(self, root):
-        forms = {}
-        for tense in ENDINGS:
-            endings = {}
-            for ending in tense:
-                endings[ending] = root + ending
-            forms[tense] = endings
-        return forms
-
 def load_verbs(filename):
     """
-    Loads a csv with verbs and returns a list of Verb objects
+    Loads a csv with verbs and returns a list of dictionaries, including verb conjugations
     """
+    print(os.listdir(os.path.dirname(filename)))
     with open(filename, newline="", encoding="utf-8") as f:
         reader = csv.DictReader(f)
         verbs = list(reader)
 
     verb_list = []
     for verb in verbs:
-        verb_list.append(Verb(verb[0], verb[1]))
-    return verbs
+        conjugations = conjugate(verb["root"])
+        verb_list.append(verb | conjugations)
+    return verb_list
+
+def conjugate(root):
+    """
+    Take a verb root, and return a dictionary containing all the conjugations
+    """
+    forms = {}
+    for tense_name, endings in ENDINGS.items():
+        person_forms = {}
+        for person_name, ending in endings.items():
+            person_forms[person_name] = root + ending
+        forms[tense_name] = person_forms
+
+        # add aspects
+        if tense_name in ASPECTS:
+            for aspect_name, aspect_suffix in ASPECTS[tense_name].items():
+                person_forms = {}
+                for person_name, ending in endings.items():
+                    person_forms[person_name] = root + aspect_suffix + ending
+                forms[aspect_name] = person_forms
+
+    return forms
 
 # ON PAGE LOAD
 make_ready() # hide loading, show content
-verbs = load_verbs("bangla_verbs.csv")
+verbs = load_verbs("./bangla_verbs.csv")
+for verb in verbs:
+    print(verb)
